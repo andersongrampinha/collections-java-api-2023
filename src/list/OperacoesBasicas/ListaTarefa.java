1467dd59a2b891e75871eca52137595e4d69f005
Anderson Grampinha <andersongrampinha@gmail.com>
2024-07-10 16:07:57 -0300

    Resolvendo desafio lista terefa e carrinho de compras
    Signed-off-by: Anderson Grampinha <andersongrampinha@gmail.com>

---
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,55 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+        for(Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTerefa = new ListaTarefa();
+
+        System.out.println("O número total de elementos na lista é:" +
+                listaTerefa.obterNumeroTotalTarefas());
+
+        listaTerefa.adicionarTarefa("Tarefa 1");
+        listaTerefa.adicionarTarefa("Tarefa 1");
+        listaTerefa.adicionarTarefa("Tarefa 2");
+        System.out.println("O número total de elementos na lista é:" +
+                listaTerefa.obterNumeroTotalTarefas());
+
+        listaTerefa.removerTarefa("Tarefa 2");
+        System.out.println("O número total de elementos na lista é:" +
+                listaTerefa.obterNumeroTotalTarefas());
+
+        listaTerefa.obterDescricoesTarefas();
+    }
+}
